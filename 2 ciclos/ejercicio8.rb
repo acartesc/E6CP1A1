cad506cd50812962ef7d96a46d86eefe5ef8e3f0
Andres <acartescc@gmail.com>
2017-08-22 21:54:37 -0300

    2 ciclos - ejercicio 8 terminado

--- 2 ciclos/ejercicio8.rb
+++ 2 ciclos/ejercicio8.rb
@@ -2,8 +2,14 @@
 # a = '1impar 2par 3impar 4par 5impar 6par 7impar 8par 9impar 10par'
 
 a = ''
+
 10.times do |i|
-	
+	i += 1
+	if i.odd?
+		a = a + " #{i}impar"
+	else
+		a = a + " #{i}par"
+	end
 end
 
 puts a
