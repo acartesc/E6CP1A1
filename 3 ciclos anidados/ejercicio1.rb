f505969db1a00a55ca84ec04e5a25375ddc81ca7
Andres <acartescc@gmail.com>
2017-08-27 16:38:21 -0300

    3 ciclos anidados - ejercicio 1 finalizado

--- 3 ciclos anidados/ejercicio1.rb
+++ 3 ciclos anidados/ejercicio1.rb
@@ -5,3 +5,13 @@ Se pide imprimir la secuencia numérica, de la siguiente forma:
 3   6   9   12
 4   8   12   16
 =end
+
+a1 = [1, 2, 3, 4]
+a2 = [2, 4, 6, 8]
+a3 = [3, 6, 9, 12]
+a4 = [4, 8, 12, 16]
+
+puts "#{a1[0]}  #{a1[1]}  #{a1[2]}  #{a1[3]} \n"
+puts "#{a2[0]}  #{a2[1]}  #{a2[2]}  #{a2[3]} \n"
+puts "#{a3[0]}  #{a3[1]}  #{a3[2]}  #{a3[3]} \n"
+puts "#{a4[0]}  #{a4[1]}  #{a4[2]}  #{a4[3]} \n"
